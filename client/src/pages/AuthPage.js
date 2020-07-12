645cb991e9f4430670f0d9379c26bc179e70ffff
Vladyslav M <vladyslav.mahdenko@ria.com>
2020-07-12 12:24:09 +0300

    Backend end (step 1) Frontend Start (step 1)

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AuthPage = () => {
+	return (
+		<div className="row">
+		<div className="col s6 offset-s3">
+			<h1>Сократить ссылку</h1>
+		</div>
+		</div>
+	)
+}
